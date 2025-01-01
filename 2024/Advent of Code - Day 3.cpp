a8ae26f91e90012cd80b7cbcb7bfda9280ce4d37
mhlow <minghanlow@gmail.com>
2025-01-01 23:08:06 +1100

    Day 3

--- 2024/Advent of Code - Day 3.cpp
+++ 2024/Advent of Code - Day 3.cpp
@@ -3,34 +3,45 @@
 #include <vector>
 #include <algorithm>
 #include "utils.cpp"
+#include <regex>
 
 int main() {
-    std::vector<std::string> lines = read_lines("Advent of Code - Day 2.txt");
+    std::vector<std::string> lines = read_lines("Advent of Code - Day 3.txt");
     // find the string mul(XX,XX) 
     int sum = 0;
+    std::regex re("mul\\((\\d+),(\\d+)\\)");
     for (std::string line : lines) {
-        for (int i = 0; i < line.size(); i++) {
-            int num1 = 0;
-            int num2 = 0;
-            // find mul first
-            if (line[i] == 'm' && line[i + 1] == 'u' && line[i + 2] == 'l') {
-                // find the first parenthesis
-                int first_parenthesis = i + 4;
-                if (line[first_parenthesis] != '(') {
-                    continue;
-                } e
-                // find the second parenthesis
-                int second_parenthesis = line.find(')', first_parenthesis);
-                // check that there are numbers separated by a comma
-                for (int j = first_parenthesis + 1; j < second_parenthesis; j++) {
-                    if (line[j] == ',') {
-                        num1 = std::stoi(line.substr(first_parenthesis + 1, j - first_parenthesis - 1));
-                        num2 = std::stoi(line.substr(j + 1, second_parenthesis - j - 1));
-                        break;
-                    }
+        std::smatch match;
+        std::string::const_iterator searchStart( line.cbegin() );
+        while ( regex_search( searchStart, line.cend(), match, re ) ) {
+            sum += std::stoi(match[1]) * std::stoi(match[2]);
+            searchStart = match.suffix().first;
+        }
+    }
+    std::cout << sum << std::endl;
+
+    // Part 2
+    // Find the most recent "do" or "don't" and apply that to future "mul"
+    sum = 0;
+    bool is_do = true;
+    std::regex regex_do("(do\\(\\)|don't\\(\\)|mul\\(\\d+,\\d+\\))");
+    for (std::string line : lines) {
+        std::smatch match;
+        std::string::const_iterator searchStart( line.cbegin() );
+        while ( regex_search( searchStart, line.cend(), match, regex_do ) ) {
+            if (match[0] == "do()") {
+                std::cout << "do" << std::endl;
+                is_do = true;
+            } else if (match[0] == "don't()") {
+                std::cout << "don't" << std::endl;
+                is_do = false;
+            } else {
+                if (is_do) {
+                    regex_search( searchStart, line.cend(), match, re );
+                    sum += std::stoi(match[1]) * std::stoi(match[2]);
                 }
             }
-            sum += num1 * num2;
+            searchStart = match.suffix().first;
         }
     }
     std::cout << sum << std::endl;
